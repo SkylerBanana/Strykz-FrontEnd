049165668fd892f6ef56229828ab83e673fde828
SkylerBanana <Skyler_Banana@protonmail.com>
2024-11-26 08:45:46 -0500

    Did Some work on the Nav

---
+++ src/components/Nav.tsx
@@ -0,0 +1,13 @@
+export default function Nav() {
+  return (
+    <nav className="float-right border-white border-2 h-full w-16 p-2">
+      <div
+        id="Friend_Icon_Div"
+        className="flex items-center justify-center flex-col hover:opacity-50"
+      >
+        <i className="bi bi-person-fill text-3xl"></i>
+        <p className="text-sm">51</p>
+      </div>
+    </nav>
+  );
+}
